019046d1883f67f9047892950ff3defea3697962
SinBin10 <bincode10@gmail.com>
2024-08-06 00:09:28 +0530

    inserting into database.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require("path");
 const app = express();
+const userSchema = require("./models/user");
 
 //middlewares
 app.set("view engine", "ejs");
@@ -12,6 +13,17 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/create", async (req, res) => {
+  let { name, email, password, age } = req.body;
+  let user = await userSchema.create({
+    username: name,
+    email,
+    password,
+    age,
+  });
+  res.send(user);
 });
 
 app.listen(3000);
